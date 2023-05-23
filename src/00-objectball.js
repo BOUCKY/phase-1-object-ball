c31f60e8ea423222585ded4156b1a87b71cd33e5
Alexis <alexisbou16@gmail.com>
2023-05-23 15:24:15 -0400

    defined gameObject

---
+++ src/00-objectball.js
@@ -0,0 +1,15 @@
+function gameObject(){
+    const obj = {
+        "home":{
+            "teamName":"Brooklyn Nets",
+            "colors":{"Black":"White"},
+            "players":{}
+        },
+        "away":{
+            "teamName":"Charlotte Hornets",
+            "colors":"",
+            "players":" "
+        }
+    }
+}
+
