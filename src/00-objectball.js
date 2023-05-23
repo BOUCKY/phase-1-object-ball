a6fbe73b4e208e7d781c318ff41ab99411edf4d5
syeda <syedaeanjum@gmail.com>
2023-05-23 16:08:20 -0400

    done

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,13 +3,15 @@ function gameObject(){
         "home":{
             "teamName":"Brooklyn Nets",
             "colors":{"Black":"White"},
-            "players":{}
-        },
+            "players":{"Alan Anderson" :{"Number" : 0}, "Reggie Evans" : {"Number" : 30}, "Brook Lopez" :{"Number" : 11}, "Mason Plumlee" :{"Number":1}, "Jason Terry" : {"Number": 31},
         "away":{
             "teamName":"Charlotte Hornets",
-            "colors":"",
-            "players":" "
-        }
+            "colors": {"Turquoise": "Purple"},
+            "players":{"Jeff Adrian":{"Number" : 4},"Bismak Biyombo":{"Number": 0},"DeSagna Diop" :{"Number": 2}, "Ben Gordon" :{"Number":8 }, "Brendan Haywood" : {"Number":33},
+          
+     }
     }
 }
-
+}
+}
+}
